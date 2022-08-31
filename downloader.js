1ca58147f42d00a645ea9f619d9a37d1f50733af
Fros1er <34234343+Fros1er@users.noreply.github.com>
2022-08-31 17:44:57 +0800

    update

--- downloader.js
+++ downloader.js
@@ -20,7 +20,7 @@ GM_addStyle(`
   padding: 22px;
   width: auto;
   max-width: 500px;
-  z-index: 999;
+  z-index: 9999;
   transform: translate(-50%, -50%);
 }
 
@@ -33,7 +33,7 @@ GM_addStyle(`
   background: transparent;
   border: 0;
   padding: 0;
-  z-index: 1000;
+  z-index: 10000;
   width: 44px;
   height: 44px;
   line-height: 44px;
@@ -43,14 +43,19 @@ GM_addStyle(`
   opacity: 0.65;
   font-size: 28px;
 }
+
+.us-popup-reader {
+    overflow-y: scroll;
+    max-height: 500px;
+}
 `);
 
 
 function generatePopupDiv() {
-    let popup = $('<div class="us-popup"></div>');
-    let inner = $('<div class="us-popup-inner"></div>');
-    let btn = $('<button type="button" class="us-popup-close">×</button>');
-    btn.on('click', ()=>{
+    let popup = $("<div class='us-popup'></div>");
+    let inner = $("<div class='us-popup-inner'></div>");
+    let btn = $("<button type='button' class='us-popup-close'>×</button>");
+    btn.on("click", ()=>{
         popup.hide();
     });
     popup.append(btn);
@@ -61,7 +66,7 @@ function generatePopupDiv() {
 
 
 (function() {
-    'use strict';
+    "use strict";
 
     let downloadLink = $(`
     <div class="note-btn">
@@ -69,24 +74,30 @@ function generatePopupDiv() {
             <path d="M3.708 7.755c0-1.111.488-1.753 1.319-1.753.681 0 1.138.47 1.186 1.107H7.36V7c-.052-1.186-1.024-2-2.342-2C3.414 5 2.5 6.05 2.5 7.751v.747c0 1.7.905 2.73 2.518 2.73 1.314 0 2.285-.792 2.342-1.939v-.114H6.213c-.048.615-.496 1.05-1.186 1.05-.84 0-1.319-.62-1.319-1.727v-.743zm6.14 0c0-1.111.488-1.753 1.318-1.753.682 0 1.139.47 1.187 1.107H13.5V7c-.053-1.186-1.024-2-2.342-2C9.554 5 8.64 6.05 8.64 7.751v.747c0 1.7.905 2.73 2.518 2.73 1.314 0 2.285-.792 2.342-1.939v-.114h-1.147c-.048.615-.497 1.05-1.187 1.05-.839 0-1.318-.62-1.318-1.727v-.743z"/>
             <path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z"/>
         </svg>
-        下载字幕
+        查看字幕
     </div>`);
 
     let popup = generatePopupDiv();
     let popupout = popup[0];
     popup = popup[1];
+    let popupSelector = $("<div />");
+    let popupReader = $("<div class='us-popup-reader'/>");
+    popupReader.hide();
+    popup.append(popupSelector);
+    popup.append(popupReader);
+    let chose = false;
 
-    let urlParameters = window.location.pathname.split('/')
-    let bvid = urlParameters.pop()
-    if (bvid == '') bvid = urlParameters.pop()
+    let urlParameters = window.location.pathname.split("/");
+    let bvid = urlParameters.pop();
+    if (bvid == "") bvid = urlParameters.pop();
 
     // insert link
     setTimeout(()=>{
         let handler = setInterval(()=>{
-            let toolbar = $('#arc_toolbar_report .toolbar-right .note-btn');
+            let toolbar = $("#arc_toolbar_report .toolbar-right .note-btn");
             if (toolbar.length != 0) {
-                popupout.appendTo($('body'));
-                toolbar.css('margin-right', '18px');
+                popupout.appendTo($("body"));
+                toolbar.css("margin-right", "18px");
                 downloadLink.insertAfter(toolbar);
                 clearInterval(handler);
             }
@@ -95,9 +106,13 @@ function generatePopupDiv() {
 
 
     // show popup and fetch information
-    downloadLink.on('click', ()=>{
+    downloadLink.on("click", ()=>{
+        if (chose) {
+            popupout.show();
+            return;
+        }
         fetch("https://api.bilibili.com/x/web-interface/view?bvid=" + bvid, {
-            credentials: 'include'
+            credentials: "include"
           })
         .then(res => res.json())
         .then(data => {
@@ -106,14 +121,29 @@ function generatePopupDiv() {
             }
             let subtitles = data.data.subtitle.list;
             if (subtitles.length == 0) {
-                popup.html("无字幕");
+                popupSelector.html("无字幕");
             } else {
+                popupSelector.html("");
                 for (let subtitle of subtitles) {
-                    popup.append($("<a>" + subtitle.subtitle_url + "</a>"))
+                    let link = $("<a>" + subtitle.lan_doc + "</a>");
+                    link.on("click", ()=>{
+                        fetch(subtitle.subtitle_url.replace('http:', 'https:'))
+                        .then(res => res.json())
+                        .then(data => {
+                            popupReader.html("");
+                            for (let line of data.body) {
+                                popupReader.append("<p>" + line.content + "</p>");
+                            }
+                            popupReader.show();
+                            popupSelector.hide();
+                            chose = true;
+                        });
+                    });
+                    popupSelector.append(link);
                 }
             }
             popupout.show();
-            console.log(data);
+            // console.log(data);
         })
         .catch(err => {
             popup.html("获取字幕信息失败");
